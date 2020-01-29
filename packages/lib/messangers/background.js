59e139037112d417bb781580aba21718b72e5cb2
allemanfredi <alle.manfredi@gmail.com>
2020-01-29 14:30:36 +0100

    chore(lib): adds messangers

---
+++ packages/lib/messangers/background.js
@@ -0,0 +1,21 @@
+export default {
+  init(duplex) {
+    this.duplex = duplex
+  }
+
+  /*func(data) {
+    this.duplex.send('popup', 'func', data, false)
+  },
+
+  func2(data) {
+    this.duplex.send(
+      'tab',
+      'tunnel',
+      {
+        action: 'func2',
+        data:
+      },
+      false
+    )
+  },*/
+}
---
+++ packages/lib/messangers/popup.js
@@ -0,0 +1,9 @@
+export default {
+  init(duplex) {
+    this.duplex = duplex
+  }
+
+  /*func() {
+    return this.duplex.send('func')
+  }*/
+}
