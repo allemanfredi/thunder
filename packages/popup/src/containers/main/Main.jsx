590f7943101b81b22d6f7899149b047efc53c242
allemanfredi <alle.manfredi@gmail.com>
2020-01-30 18:26:38 +0100

    chore(popup): <- refactors component of this

--- packages/popup/src/containers/main/Main.jsx
+++ packages/popup/src/containers/main/Main.jsx
@@ -10,23 +10,23 @@ const Main = props => {
     <Fragment>
       <Header />
       <hr />
-      <div className='main__body-container'>
-        <div className='main__body-container-toogle-button'>
+      <div className="main__body-container">
+        <div className="main__body-container-toogle-button">
           <Switch
             checked={toogleEnabled}
             onChange={() => {
               setToogleEnabled(!toogleEnabled)
             }}
-            onColor='#888888'
-            onHandleColor='#ffa040'
+            onColor="#888888"
+            onHandleColor="#ffa040"
             handleDiameter={30}
             uncheckedIcon={false}
             checkedIcon={false}
-            boxShadow='0px 1px 5px rgba(0, 0, 0, 0.6)'
-            activeBoxShadow='0px 0px 1px 10px rgba(0, 0, 0, 0.2)'
+            boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+            activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
             height={20}
             width={48}
-            className='react-switch'
+            className="react-switch"
           />
         </div>
       </div>
