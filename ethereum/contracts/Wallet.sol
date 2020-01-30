309864cebec1b4c3b5624c5657e20133887a4582
allemanfredi <alle.manfredi@gmail.com>
2020-01-30 14:53:01 +0100

    feat(ethereum): adds new PR fx in sc

--- ethereum/contracts/Wallet.sol
+++ ethereum/contracts/Wallet.sol
@@ -13,16 +13,38 @@ contract Wallet {
       address creator;
       uint256 bounty;
   }
+  
+  struct PullRequest {
+      bytes32 id;
+      uint64 issueNumber;
+      address creator;
+  }
 
   mapping(address => Repo[]) repoOwner;
   mapping(bytes32 => Issue[]) repoIssues;
   mapping(bytes32 => uint256) issueBounty;
   mapping(bytes32 => bool) hasBountyOption;
+  mapping(bytes32 => address) repoIdOwner;
+  mapping(bytes32 => bytes32) issueIdRepoId;
+  mapping(bytes32 => mapping(uint64 => PullRequest[])) repoPullRequests;
+  mapping(bytes32 => address) pullRequestIdCreator;
+  mapping(address => bytes32) creatorPullRequestId;
 
   event NewRepoEvent(string repoOwner, string repoName, bytes32 repoId);
-  event NewIssueEvent(string repoOwner, string repoName, bytes32 issueId, uint256 boutny);
+  event NewIssueEvent(string repoOwner, string repoName, bytes32 issueId, uint256 bounty);
+  event NewPullRequestEvent(address pullRequestCreator, string repoName, uint64 issueId);
 
   constructor() public {}
+  
+  modifier onlyRepoOwner(
+      string memory _repoOwner,
+      string memory _repoName
+  )
+  {
+      bytes32 repoId = keccak256(abi.encodePacked(_repoName, _repoOwner));
+      require(msg.sender == repoIdOwner[repoId], 'This function is callable only by the repo owner');
+      _;
+  }
 
   function newRepo (
       string memory _repoOwner,
@@ -34,6 +56,7 @@ contract Wallet {
       bytes32 repoId = keccak256(abi.encodePacked(_repoName, _repoOwner));
       repoOwner[msg.sender].push(Repo(repoId, _repoName, _repoOwner));
       hasBountyOption[repoId] = true;
+      repoIdOwner[repoId] = msg.sender;
       emit NewRepoEvent(_repoOwner, _repoName, repoId);
       return true;
   }
@@ -48,10 +71,15 @@ contract Wallet {
       returns (bool)
   {
       require(msg.value > 0, "Issue bounty must be greater than 0");
+      
       bytes32 issueId = keccak256(abi.encodePacked(_issueNumber, _repoName, _repoOwner));
       bytes32 repoId = keccak256(abi.encodePacked(_repoName, _repoOwner));
+      
+      require(issueIdRepoId[issueId] == 0, "Issue already existing");
+      
       repoIssues[repoId].push(Issue(issueId, _issueNumber, msg.sender, msg.value));
       issueBounty[issueId] = msg.value;
+      issueIdRepoId[issueId] = repoId;
       emit NewIssueEvent(_repoOwner, _repoName, issueId, msg.value);
       return true;
   }
@@ -79,5 +107,30 @@ contract Wallet {
   {
       bytes32 repoId = keccak256(abi.encodePacked(_repoName, _repoOwner));
       return hasBountyOption[repoId];
+  }
+  
+  /**
+   * User can do only a PR on an issue (for now)
+   * */
+  function newPullRequest (
+      string memory _repoOwner,
+      string memory _repoName,
+      uint64 _issueNumber
+  )
+      public
+      returns (bool)
+  {
+      bytes32 issueId = keccak256(abi.encodePacked(_issueNumber, _repoName, _repoOwner));
+      bytes32 repoId = keccak256(abi.encodePacked(_repoName, _repoOwner));
+      require(issueIdRepoId[issueId] == repoId, "Issue not existing");
+      
+      bytes32 pullRequestId = keccak256(abi.encodePacked(_repoName, _repoOwner, _issueNumber, msg.sender));
+      require(pullRequestIdCreator[pullRequestId] != msg.sender, "msg.sender has already done a PR on this issue");
+      
+      repoPullRequests[repoId][_issueNumber].push(PullRequest(pullRequestId, _issueNumber, msg.sender));
+      pullRequestIdCreator[pullRequestId] = msg.sender;
+      creatorPullRequestId[msg.sender] = pullRequestId;
+      emit NewPullRequestEvent(msg.sender, _repoName, _issueNumber);
+      return true;
   }
 }
