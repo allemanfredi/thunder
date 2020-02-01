d0f720ec87201d1f4c558526a7258108eb72978c
allemanfredi <alle.manfredi@gmail.com>
2020-02-01 18:37:29 +0100

    feat(inpage): adds sc interaction for getting user info

--- packages/inpage/pages/profile.js
+++ packages/inpage/pages/profile.js
@@ -1,17 +1,15 @@
-import { extrapolateIssueNumberFromText } from '@thunder/lib/utils'
-import { makeEthContractSend, makeEthContractCall } from '@thunder/lib/eth'
-import gh from 'parse-github-url'
-
-const pricesColor = {
-  low: 'rgb(255, 205, 86)',
-  normal: 'rgb(54, 162, 235)',
-  high: 'rgb(255, 159, 64)'
-}
+import { makeEthContractCall } from '@thunder/lib/eth'
 
 const Profile = {
   form: null,
 
   async injectElements(_web3, _url) {
+    const username = document.querySelector(
+      '#js-pjax-container > div > div.h-card.col-lg-3.col-md-4.col-12.float-md-left.pr-md-3.pr-xl-6 > div.clearfix.mb-2 > div.float-left.col-9.col-md-12.pl-2.pl-md-0 > div.vcard-names-container.pt-1.pt-md-3.pb-1.pb-md-3.js-sticky.js-user-profile-sticky-fields > h1 > span.p-nickname.vcard-username.d-block'
+    ).innerText
+
+    const info = await makeEthContractCall(_web3, 'getUsernameInfo', [username])
+
     document
       .querySelector(
         '#js-pjax-container > div > div.h-card.col-lg-3.col-md-4.col-12.float-md-left.pr-md-3.pr-xl-6 > div.clearfix.mb-2 > div.float-left.col-9.col-md-12.pl-2.pl-md-0 > div.vcard-names-container.pt-1.pt-md-3.pb-1.pb-md-3.js-sticky.js-user-profile-sticky-fields > h1'
@@ -20,13 +18,13 @@ const Profile = {
         'afterend',
         `
       <hr/>
-      <span style="
+      <span style='
       background: rgb(255, 159, 64, 0.45);
       padding: 7px;
       border: 3px solid rgb(255, 159, 64);
       border-radius: 5px;
-      font-weight: 600;">
-        Reputation: 0
+      font-weight: 600;'>
+        Reputation: ${info[1]}  |  #Issues: ${info[0]}
       </span>
       <hr/>
     `
