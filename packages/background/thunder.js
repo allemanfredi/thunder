2b99a5cd89d36efd7f7f2a81197c2e153b624f4d
allemanfredi <alle.manfredi@gmail.com>
2020-02-04 19:19:25 +0100

    chore(background): changes class name

--- packages/background/thunder.js
+++ packages/background/thunder.js
@@ -1,21 +1,21 @@
 import { backgroundMessanger } from '@thunder/lib/messangers'
 import GithubApiController from './controllers/github-api-controller'
-import PersistenceStorageController from './controllers/persistence-global-state-controller'
+import PersistenceGlobalStateController from './controllers/persistence-global-state-controller'
 
 class Thunder {
   constructor(duplex) {
     backgroundMessanger.init(duplex)
 
     this.githubApiController = new GithubApiController()
-    this.persistenceStorageController = new PersistenceStorageController()
+    this.persistenceStorageController = new PersistenceGlobalStateController()
   }
 
   getGlobalState() {
-    return this.persistenceStorageController.state
+    return this.persistenceGlobalStateController.state
   }
 
   changeEnabling(_isEnabled) {
-    this.persistenceStorageController.changeEnabling(_isEnabled)
+    this.persistenceGlobalStateController.changeEnabling(_isEnabled)
   }
 }
 
